617f823d86a741c2ca11e085271c2cdd14ed69d5
mia-hlynchuk <solomiya.hlynchuk@gmail.com>
2019-06-19 17:12:52 -0400

    Edited <td> xpath and added Regexes
    Included exceptions to <td> elements xpaths. Removed unwanted crew names from <td> data

--- movies_list_v2_spider.py
+++ movies_list_v2_spider.py
@@ -2,6 +2,7 @@
 # different layout of the wikitables after the 2013 year
 
 import scrapy
+import re
 
 YEAR = '2014'
 URL = 'http://en.wikipedia.org/wiki/List_of_American_films_of_'+ YEAR
@@ -26,6 +27,10 @@ class MovieSpider(scrapy.Spider):
     for movie in movies_selector:  
       # select <td> elements with rowspan attribute only
       td_rowspan = movie.xpath('td[@rowspan]')
+
+      # a few <td> elements have align or style attribute instead of rowspan
+      td_align = movie.xpath('td[1][@align]')
+      td_style = movie.xpath('td[1][@style]')
         
       # the default index for the xpath for the <td> elements
       td_name_index = '1' 
@@ -36,23 +41,36 @@ class MovieSpider(scrapy.Spider):
         td_name_index = '3'
         td_cast_index = '5'
         td_genre_index = '6'
-      elif len(td_rowspan) == 1:
+      elif len(td_rowspan) == 1 or td_align or td_style:
         td_name_index = '2'
         td_cast_index = '4'
         td_genre_index = '5'
  
       name = movie.xpath('td['+ td_name_index +']/descendant-or-self::text()').extract()[0]
-      cast_selector = movie.xpath('td['+ td_cast_index +']/descendant-or-self::text()').extract()
+
+      cast = movie.xpath('td['+ td_cast_index +']/descendant-or-self::text()').extract()
+      # join the cast list into a string
+      cast_string = ' '.join(cast)
+      # since the cast column changed to include the director and screenplay people, 
+      # we need to remove them before storing the actors' names
+      # get the beginning of the string till the end of ')'  
+      to_remove = re.findall(r"^.*\(.*?\)", cast_string)
+      if to_remove:
+        # split the string back to a list
+        updated_cast = cast_string.replace(to_remove[0], '').split(",")
+      else:
+        updated_cast = cast
+     
       # seperate each actor into its own object
-      cast = []
-      for actor in cast_selector:
-        cast.append({'name': actor }) # need to include id
+      castArray = []
+      for actor in updated_cast:
+        castArray.append({'name': actor.strip() }) # need to include id
       
       genre = movie.xpath('td['+ td_genre_index +']/descendant-or-self::text()').extract()
 
       yield MovieItem(
         name = name,
-        cast = cast,
+        cast = castArray,
         genre = genre,
         year = YEAR
       )   
