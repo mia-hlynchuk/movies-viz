9c64f2fb5328bd2fee0ef312e626939d6f21c830
mia-hlynchuk <solomiya.hlynchuk@gmail.com>
2019-06-22 01:50:51 -0400

    Added conditions to exclude certain data

--- movies_list_spider.py
+++ movies_list_spider.py
@@ -32,24 +32,27 @@ class MovieSpider(scrapy.Spider):
     movies = response.xpath('//*[@id="mw-content-text"]/div/table[contains(@class, "wikitable")]/tbody/tr[position()>1]')
 
     for movie in movies:
-      #name_selector = movie.xpath('td[1]/i/a/text()').extract()[0]
-      # in 2012 and 2013 tables some movie names and cast are more nested 
-      name = movie.xpath('td[1]/i/descendant-or-self::text()').extract()[0] 
-      cast = movie.xpath('td[3]/descendant-or-self::text()').extract()
-     
-      # need to do this because the cast returns ',' as an array item
-      updated_cast = ''.join(cast).split(', ')
-      
-      # seperate each actor into its own object
-      castArray = []
-      for actor in updated_cast:
-        castArray.append({'name': actor }) # need to include id
-      
-      genre = movie.xpath('td[4]/descendant-or-self::text()').extract()
+      # exclude documentary/concert genre
+      _genre = movie.xpath('td[4]/descendant-or-self::text()').extract()
+      genre = ''.join(_genre).split(', ')
 
-      yield MovieItem(
-        name = name,
-        cast = castArray,
-        genre = genre,
-        year = YEAR
-      )   
+      if len(genre)==0 or not (any(x in ['Documentary','Nature documentary','Concert'] for x in genre)):
+        #name_selector = movie.xpath('td[1]/i/a/text()').extract()[0]
+        # in 2012 and 2013 tables some movie names and cast are more nested 
+        name = movie.xpath('td[1]/i/descendant-or-self::text()').extract()[0] 
+        cast = movie.xpath('td[3]/descendant-or-self::text()').extract()
+      
+        # need to do this because the cast returns ',' as an array item
+        updated_cast = ''.join(cast).split(', ')
+        
+        # seperate each actor into its own object
+        castArray = []
+        for actor in updated_cast:
+          castArray.append({'name': actor }) # need to include id
+        
+        yield MovieItem(
+          name = name,
+          cast = castArray,
+          genre = genre,
+          year = YEAR
+        )   
--- movies_list_v2_spider.py
+++ movies_list_v2_spider.py
@@ -4,8 +4,9 @@
 import scrapy
 import re
 
-YEAR = '2014'
 URL = 'http://en.wikipedia.org/wiki/List_of_American_films_of_'
+_year = '2018'
+
 
 # define the data that will be scraped
 class MovieItem(scrapy.Item):
@@ -20,17 +21,19 @@ class MovieSpider(scrapy.Spider):
   name = 'movies_list_v2'
   allowed_domains = ['en.wikipedia.org']
 
-  # passing an argument to the spider 
+  # passing an argument to the spider
   # in the terminal type '-a year=2000'
   def __init__(self, year=None, *args, **kwargs):
     super(MovieSpider, self).__init__(*args, **kwargs)
-    YEAR = str(year)
-    self.start_urls = [URL + YEAR]
+    #year = str(_year)
+    self.start_urls = [URL + year]
 
   # deal with the http response
   def parse(self, response):
     movies_selector = response.xpath('//*[@id="mw-content-text"]/div/table[contains(@class, "wikitable")]/tbody/tr[position()>1]')
-    for movie in movies_selector:  
+    tr_i = 0
+    for movie in movies_selector: 
+      tr_i += 1 
       # select <td> elements with rowspan attribute only
       td_rowspan = movie.xpath('td[@rowspan]')
 
@@ -43,40 +46,57 @@ class MovieSpider(scrapy.Spider):
       td_cast_index = '3'
       td_genre_index = '4'
 
-      if len(td_rowspan) == 2:
+      if len(td_rowspan) == 2 or (_year=='2015' and (tr_i==113 or tr_i==114)):
         td_name_index = '3'
         td_cast_index = '5'
         td_genre_index = '6'
-      elif len(td_rowspan) == 1 or td_align or td_style:
+      elif len(td_rowspan) == 1 or td_align or td_style or (_year=='2015' and (tr_i==120 or tr_i==126 or tr_i==139)):
         td_name_index = '2'
         td_cast_index = '4'
         td_genre_index = '5'
- 
-      name = movie.xpath('td['+ td_name_index +']/descendant-or-self::text()').extract()[0]
 
       cast = movie.xpath('td['+ td_cast_index +']/descendant-or-self::text()').extract()
-      # join the cast list into a string
-      cast_string = ' '.join(cast)
-      # since the cast column changed to include the director and screenplay people, 
-      # we need to remove them before storing the actors' names
-      # get the beginning of the string till the end of ')'  
-      to_remove = re.findall(r"^.*\(.*?\)", cast_string)
-      if to_remove:
-        # split the string back to a list
-        updated_cast = cast_string.replace(to_remove[0], '').split(",")
-      else:
-        updated_cast = cast
-     
-      # seperate each actor into its own object
-      castArray = []
-      for actor in updated_cast:
-        castArray.append({'name': actor.strip() }) # need to include id
-      
       genre = movie.xpath('td['+ td_genre_index +']/descendant-or-self::text()').extract()
 
-      yield MovieItem(
-        name = name,
-        cast = castArray,
-        genre = genre,
-        year = YEAR
-      )   
+      # remove the empty quotes from the cast and the genre lists
+      for a_list in [cast, genre]:
+        while '\n' in a_list:
+          a_list.remove('\n')
+        while ' ' in a_list:
+          a_list.remove(' ')
+        # for 2017 and up
+        while ', ' in a_list:
+          a_list.remove(', ')
+      
+      # exclude documentary and concert related genres
+      if len(genre)==0 or not (any(x in ['Documentary','Nature documentary','Concert','Political documentary','Biopic Documentary'] for x in genre)):
+        name = movie.xpath('td['+ td_name_index +']/descendant-or-self::text()').extract()[0]
+        
+        # for 2017 and up the cast column changed to include the director and screenplay people, 
+        # therefore we need to remove them before storing the actors' names
+        if int(_year) >= 2017:
+          # seperate the genre string into a list
+          updated_genre = genre[0].replace(r'\\n', '').split(',')
+
+          # join the cast list into a string
+          cast_string = ','.join(cast)
+          # get the beginning of the string till the end of ';'  
+          to_remove = re.findall(r"^.*\(.*?\)\;\s,", cast_string)
+          if to_remove:           
+            # split the string back to a list
+            updated_cast = cast_string.replace(to_remove[0], '').split(',')
+        else:
+          updated_cast = cast
+          updated_genre = genre
+      
+        # seperate each actor into its own object
+        castArray = []
+        for actor in updated_cast:
+          castArray.append({'name': actor }) # need to include id
+          
+        yield MovieItem(
+          name = name,
+          cast = castArray,
+          genre = updated_genre,
+          year = _year
+        )   
