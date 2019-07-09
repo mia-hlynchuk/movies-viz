d987c05b3c3648700072ebe5c937c61387839a4e
mia-hlynchuk <solomiya.hlynchuk@gmail.com>
2019-07-09 11:36:32 -0400

    Updated the way the actors get their id

--- loading_json_data.py
+++ loading_json_data.py
@@ -1,5 +1,5 @@
 # based on the year selected, edit/sort the movies' and the actors' ids
-# in the command line type: loading_data.py 2002
+# in the command line type: loading_json_data.py 2002
 import sys
 import json
 
@@ -7,7 +7,6 @@ import json
 all_actors = []
 # give each movie and actor a unique id
 movie_id = 0
-actor_id = 0
 file_to_load = ''
 
 def file_year(year):
@@ -23,32 +22,35 @@ if __name__ == "__main__":
 print("FILE TO LOAD:")
 print(file_to_load)
 print("===================")
-with open(file_to_load, 'r+') as f:
-  movies = json.load(f)
-   
 
+with open(file_to_load, 'r+') as f:
+  # load the year file that we want to edit
+  movies = json.load(f) 
   for movie in movies:
     movie['id'] = 'm-' + str(movie_id)
 
-    m_name = movie['name']
-    print('MOVIE:' + m_name)
-    
     cast = movie['cast']
     for actor in cast:
-      actor['id'] = 'a-' + str(actor_id)
-      # add the actor to the main list of actors
-      all_actors.append(actor)
-      actor_id += 1
+      # only add the actor that  is not in the all_actors list
+      if not (actor['name'] in all_actors):
+        all_actors.append(actor['name'])
+        
+      # find the index of that actor and make it into its id
+      actor['id'] = 'a-' + str(all_actors.index(actor['name']))
     # end of actors for loop
 
     movie_id += 1  
   # end of movies for loop
 
-  print('last movie id: ' + str(movie_id)) 
-  print('last actor id: ' + str(actor_id)) 
+  print( 'last movie id: ' + str(movie_id) ) 
+  print( 'last actor id: ' + str(len(all_actors)) )
 
   #reset file position to the beginning
   f.seek(0)
-  json.dump(movies, f) 
+  json.dump(movies, f, indent=2) 
   f.truncate()
-    
+
+  # create/dump all_actors into a json file
+  # will store all the actors from all the years files
+  with open("actors.json","a+") as af:
+    json.dump(all_actors, af, indent=2)
