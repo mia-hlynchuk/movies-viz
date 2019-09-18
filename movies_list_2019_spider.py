7ff64a9b1b6ecae14d17cc638236ba26c7dfb790
mia-hlynchuk <solomiya.hlynchuk@gmail.com>
2019-09-18 01:55:30 -0400

    Update movies_list_2019_spider.py

--- movies_list_2019_spider.py
+++ movies_list_2019_spider.py
@@ -3,7 +3,7 @@
 import scrapy
 import re
 
-URL = 'https://en.wikipedia.org/wiki/2019_in_film'
+URL = 'https://en.wikipedia.org/wiki/List_of_American_films_of_2019'
 
 # define the data that will be scraped
 class MovieItem(scrapy.Item):
@@ -21,50 +21,45 @@ class MovieSpider(scrapy.Spider):
 
   # deal with the http response
   def parse(self, response):
-    movies = response.xpath('//*[@id="mw-content-text"]/div/table[contains(@class, "wikitable")][position()>5]/tbody/tr[position()>1]')
+    movies = response.xpath('//*[@id="mw-content-text"]/div/table[contains(@class, "wikitable")]/tbody/tr[position()>1]')  
     
     for movie in movies:
-      # select <td> elements with rowspan attribute only
       td_rowspan = movie.xpath('td[@rowspan]')
-
-      # a few <td> elements have align or style attribute instead of rowspan
-      td_align = movie.xpath('td[1][@align]')
       td_style = movie.xpath('td[1][@style]')
-        
+
       # the default index for the xpath for the <td> elements
       td_name_index = '1' 
       td_cast_index = '3'
       td_genre_index = '4'
 
-      if len(td_rowspan) == 2:
-        td_name_index = '3'
-        td_cast_index = '5'
-        td_genre_index = '6'
-      elif len(td_rowspan) == 1 or td_align or td_style:
+      if td_rowspan or td_style:
         td_name_index = '2'
         td_cast_index = '4'
         td_genre_index = '5'
-    
+
       cast = movie.xpath('td['+ td_cast_index +']/descendant-or-self::text()').extract()
-      genre = movie.xpath('td['+ td_genre_index +']/text()').extract()
-  
-      if len(genre)==0 or not (any(x in ['Documentary','Nature documentary','Concert'] for x in genre)):
-        name = movie.xpath('td['+ td_name_index +']/i/a/text()').extract()[0]
-        
-        # seperate the genre string into a list
-        updated_genre = genre[0].replace(r'\\n', '').split(',')
+      genre = movie.xpath('td['+ td_genre_index +']/text()').extract()[0]
+
+
+      # seperate the genre string into a list
+      genre = genre.replace(r'\\n', '').split(',')
+
+      if not 'Documentary' in genre:
+        name = movie.xpath('td['+ td_name_index +']/descendant-or-self::text()').extract()[0]
+        #print(name)      
 
         # join the cast list into a string
-        cast_string = ','.join(cast)
+        cast_string = ','.join(cast).strip()
+        
         # get the beginning of the string till the end of ';'  
-        to_remove = re.findall(r"^.*\(.*?\)\;\s,", cast_string)
+        to_remove = re.findall(r"^.*\(.*?\)\;", cast_string)
         if to_remove:           
           # split the string back to a list
           updated_cast = cast_string.replace(to_remove[0], '').split(',')
         else:
           updated_cast = cast
-          updated_genre = genre
-
+          
+        
         # remove the empty spaces in the updated cast list
         while "\n" in updated_cast:
           updated_cast.remove("\n")
@@ -72,7 +67,7 @@ class MovieSpider(scrapy.Spider):
           updated_cast.remove(" ")
         while "" in updated_cast:
           updated_cast.remove("")
-
+        
         # seperate each actor into its own object
         castArray = []
         for actor in updated_cast:
@@ -81,6 +76,6 @@ class MovieSpider(scrapy.Spider):
         yield MovieItem(
           name = name,
           cast = castArray,
-          genre = updated_genre,
+          genre = genre,
           year = 2019
         )
