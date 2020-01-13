26eb36dd99a7e5f5004c75f6f4a13647937e551d
mia-hlynchuk <solomiya.hlynchuk@gmail.com>
2020-01-12 22:32:56 -0500

    Create merge_files.py
    merge all the movies list files into one json file

---
+++ merge_files.py
@@ -0,0 +1,25 @@
+# merge all the movies_list_* files into one
+# by looping through the json files 
+# and appending each movie to a new array
+# and then writing the new array into the new json file
+
+import glob
+import json
+
+# will store all the movies
+all_data = []
+
+# get all the files starting with 'movies_list_' in their name 
+for file in glob.glob('movies_list_*[0-9]*.json'):
+  print(file)
+
+  # load the data from the json file
+  with open (file) as json_file:
+    movies_data = json.load(json_file)
+
+    # add each movie into the all_data array
+    for movie in movies_data:
+      all_data.append(movie)
+
+with open('all_movies.json', 'w') as f:
+  json.dump(all_data, f, indent=2)
